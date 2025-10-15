ad59667d4f821f75329cad260c3b74b081cb4117
stukalomykola <stukalo.mykola23@gmail.com>
2025-10-15 20:58:08 +0300

    1

---
+++ робота 2.py
@@ -0,0 +1,48 @@
+#Додавання
+i1 = 2
+i2 = 3
+f1 = 2.1
+f2 = 3.1
+print(i1 + i2)
+print(f1 + f2)
+print(i1 + f2)
+print(type(i1 + f2))
+
+
+#Віднімання
+i1 = 2
+i2 = 3
+f1 = 2.1
+f2 = 3.1
+print(i1 - i2)
+print(f1 - f2)
+print(f2 - i1)
+print(type(f2 - i1))
+
+
+#Множення
+i1 = 2
+i2 = 2
+f1 = 2.5
+f2 = 2.5
+print(i1 * i2)
+print(f1 * f2)
+print(f1 * i1)
+print(type(f1 * i1))
+
+
+#Ділення
+i1 = 4
+i2 = 2
+f1 = 2.5
+f2 = 1.25
+print(i1 / i2)
+print(f1 / f2)
+print(f1 / i2)
+print(type(f1 / i2))
+
+
+#Піднесення у ступінь
+print(2 ** 2)
+print(3 ** 2)
+print(2 ** 3)
